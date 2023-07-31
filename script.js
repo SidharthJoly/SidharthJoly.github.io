547c31c9468bf32068d94f1599731251c42b2d19
Sidharth Joly <sidharthjoly@mail.com>
2023-07-31 13:27:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-var kinet = new Kinet({
-  acceleration: 0.06,
-  friction: 0.20,
-  names: ["x", "y"],
-});
-var circle = document.getElementById('circle');
-kinet.on('tick', function(instances) {
-  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
-});
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function () {
+  var kinet = new Kinet({
+    acceleration: 0.06,
+    friction: 0.20,
+    names: ["x", "y"],
+  });
 
-function getMousePosition(event) {
-  var rect = circle.getBoundingClientRect();
-  var mouseX = event.clientX - rect.left - rect.width / 2 + window.scrollX;
-  var mouseY = event.clientY - rect.top - rect.height / 2 + window.scrollY;
-  return { x: mouseX, y: mouseY };
-}
+  var circle = document.getElementById('circle');
+  kinet.on('tick', function (instances) {
+    circle.style.transform = `translate3d(${instances.x.current}px, ${instances.y.current}px, 0) rotateX(${instances.x.velocity / 2}deg) rotateY(${instances.y.velocity / 2}deg)`;
+  });
 
-document.addEventListener('mousemove', function (event) {
-  var mousePosition = getMousePosition(event);
-  kinet.animate('x', mousePosition.x);
-  kinet.animate('y', mousePosition.y);
-});
+  function getMousePosition(event) {
+    var mouseX = event.pageX - window.innerWidth / 2;
+    var mouseY = event.pageY - window.innerHeight / 2 + window.scrollY;
+    return { x: mouseX, y: mouseY };
+  }
 
-kinet.on('start', function() {
-  console.log('start');
-});
+  document.addEventListener('mousemove', function (event) {
+    var mousePosition = getMousePosition(event);
+    kinet.animate('x', mousePosition.x);
+    kinet.animate('y', mousePosition.y);
+  });
 
-kinet.on('end', function() {
-  console.log('end');
+  kinet.on('start', function () {
+    console.log('start');
+  });
+
+  kinet.on('end', function () {
+    console.log('end');
+  });
 });
